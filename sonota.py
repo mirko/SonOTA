a9e9f17485654e84519a8ac01189b8cd2353e84d
Sillyfrog <sillyfrog@users.noreply.github.com>
2017-08-23 20:17:54 +1000

    Update to step user through the process

--- sonota.py
+++ sonota.py
@@ -32,6 +32,11 @@ from time import sleep, time
 from uuid import uuid4
 from hashlib import sha256
 from socket import error as socket_error
+import os
+import sys
+import threading
+import netifaces
+import _thread
 
 logfmt = '%(asctime)s (%(levelname)s) %(message)s'
 loglvl = logging.DEBUG
@@ -45,19 +50,21 @@ logger = logging.getLogger(__name__)
 # we can convert the ELF binaries into v2 images with an undocumented
 #   '--version' switch to elf2image using esptool.py, e.g.:
 #     esptool.py elf2image --version 2 /tmp/arduino_build_XXXXXX/sonoff.ino.elf
-default_port = 4223
+DEFAULT_PORT_HTTPS = 8443
+DEFAULT_PORT_HTTP = 8080
 upgrade_file_user1 = "image_user1-0x01000.bin"
 upgrade_file_user2 = "image_user2-0x81000.bin"
+arduino_file = "image_arduino.bin"
 
 
 # -----
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("serving_host", help="The host's ip address which will handle the HTTP(S)/WebSocket requests initiated by the device.\
+parser.add_argument("--serving-host", help="The host's ip address which will handle the HTTP(S)/WebSocket requests initiated by the device.\
  Normally the ip address of the WiFi interface of the machine this script is running on.")
 parser.add_argument("--no-prov", help="Do not provision the device with WiFi credentials.\
- Only use if your device is already configured.", action="store_true")
+ Only use if your device is already configured (Not recommended).", action="store_true")
 parser.add_argument(
     "--wifi-ssid",
     help="The ESSID of the WiFi network the device should eventually connect to.")
@@ -69,9 +76,8 @@ parser.add_argument("--legacy", action="store_true", help="Enable legacy mode fo
                     "(requires root permission)")
 args = parser.parse_args()
 
-
 if args.legacy:     # requires root permission
-    default_port = 443
+    DEFAULT_PORT_HTTPS = 443
 
 if args.no_prov and (args.wifi_ssid or args.wifi_password):
     parser.error(
@@ -80,10 +86,6 @@ if args.no_prov and (args.wifi_ssid or args.wifi_password):
 if (args.wifi_ssid or args.wifi_password) and not (args.wifi_ssid and args.wifi_password):
     parser.error(
         "arguments --wifi-ssid and --wifi-password must always occur together")
-
-if not args.no_check_ip:
-    import netifaces
-
 
 class OTAUpdate(tornado.web.StaticFileHandler):
     # Override tornado.web.StaticHandler to debug-print GET request
@@ -105,7 +107,7 @@ class DispatchDevice(tornado.web.RequestHandler):
             "error": 0,
             "reason": "ok",
             "IP": args.serving_host,
-            "port": default_port
+            "port": DEFAULT_PORT_HTTPS
         }
         print(">> %s" % self.request.path)
         print(">> %s" % json.dumps(data, indent=4))
@@ -289,7 +291,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
                         # as we don't know which partition is (in)active, we
                         # provide our custom image as user1 as well as user2.
                         # unfortunately this also means that our firmware image
-                        # must noch exceed FLASH_SIZE / 2 - (bootloader - spiffs)
+                        # must not exceed FLASH_SIZE / 2 - (bootloader - spiffs)
                         # TODO: figure out how to build user1 and user2 incl.
                         #   its necessary differences
                         # TODO: check whether orig. bootloader can load and boot
@@ -298,16 +300,16 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
                         # images
                         "binList": [
                             {
-                                "downloadUrl": "http://%s:8080/ota/%s" %
-                                (args.serving_host, upgrade_file_user1),
+                                "downloadUrl": "http://%s:%s/ota/%s" %
+                                (args.serving_host, DEFAULT_PORT_HTTP, upgrade_file_user1),
                                 # the device expects and checks the sha256 hash of
                                 #   the transmitted file
                                 "digest": hash_user1,
                                 "name": "user1.bin"
                             },
                             {
-                                "downloadUrl": "http://%s:8080/ota/%s" %
-                                (args.serving_host, upgrade_file_user2),
+                                "downloadUrl": "http://%s:%s/ota/%s" %
+                                (args.serving_host, DEFAULT_PORT_HTTP, upgrade_file_user2),
                                 # the device expects and checks the sha256 hash of
                                 #   the transmitted file
                                 "digest": hash_user2,
@@ -342,7 +344,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 def make_app():
     return tornado.web.Application([
-        # handling initial dispatch SHTTP POST call to eu-disp.coolkit.cc
+        # handling initial dispatch HTTPS POST call to eu-disp.coolkit.cc
         (r'/dispatch/device', DispatchDevice),
         # handling actual payload communication on WebSockets
         (r'/api/ws', WebSocketHandler),
@@ -353,51 +355,141 @@ def make_app():
         (r'/ota/(.*)', tornado.web.StaticFileHandler, {'path': "static/"})
     ])
 
+def defaultinterface():
+    '''The interface the default gateway is on, if there is one.'''
+    try:
+        return netifaces.gateways()['default'][netifaces.AF_INET][1]
+    except:
+        return None
 
-def main():
-    app = make_app()
+def ip4ips():
+    '''A list of IP4 addresses on this host.
 
+    This will try and return the default gateway first in the list, and will
+    strip out localhost addresses automatically.
+    '''
+    ret = []
+    defiface = defaultinterface()
+    for iface in netifaces.interfaces():
+        addresses = netifaces.ifaddresses(iface)
+        if netifaces.AF_INET not in addresses:   # python3
+            continue
+        for afinet in addresses[netifaces.AF_INET]:
+            addr = afinet['addr']
+            if addr.startswith('127.'):
+                continue
+            if iface == defiface:
+                ret.insert(0, addr)
+            else:
+                ret.append(addr)
+    return ret
+
+def hassonoffip():
+    '''True if one of the current system IP's is from the Sonoff'''
+    for addr in ip4ips():
+        if addr.startswith("10.10.7."):
+            return True
+    return False
+
+def hasfinalstageip():
+    '''True if one of the current system IP's is from the final stage image'''
+    return '192.168.4.2' in ip4ips()
+
+def promptforval(msg):
+    while True:
+        val = input('{}: '.format(msg)).strip()
+        if val:
+            return val
+
+def checkargs():
+    # Make sure all of the binary files that are needed are there
+    for fn in [arduino_file, upgrade_file_user1, upgrade_file_user2]:
+        fn = os.path.join('static', fn)
+        if not os.path.isfile(fn):
+            print("Required file missing!", fn)
+            sys.exit(1)
+        f = open(fn, 'rb').read()
+        if len(f) < 100000:
+            print("Binary file appears too small!", fn)
+            sys.exit(1)
+
+    # Get the serving IP
+    while not args.serving_host:
+        ips = ip4ips()
+        i = 0
+        print("Select IP address of the WiFi interface:")
+        for ip in ips:
+            print("    {}: {}".format(i, ip))
+        selection = input('Select IP address [0]: ').strip()
+        if selection == '':
+            args.serving_host = ips[0]
+        else:
+            try:
+                args.serving_host = ips[int(selection)]
+            except:
+                pass
+
+    # Ensure the given IP is actually what is associated with the host
+    # and not already on a Sonoff WiFi network.
+    if args.serving_host not in ip4ips():
+        print(
+            "** The IP address of <serve_host> (%s) is not " % args.serving_host,
+            "assigned to any interface on this machine.")
+        print(
+            "** Please change WiFi network to %s and make sure %s is " % (
+                args.wifi_ssid, args.serving_host),
+            "being assigned to your WiFi interface before connecting to the ",
+            "Sonoff device.")
+        sys.exit(1)
+    if hasfinalstageip() or hassonoffip():
+        print('It looks like you are already on a Sonoff/Final stage WiFi '\
+            'network, please change to your normal WiFi network. If this '\
+            'has already been done, you may need to modify the IP range of '\
+            'your LAN to use this tool safely.')
+        sys.exit(1)
+
+    # Check there is a WiFi config
+    if not args.wifi_ssid:
+        args.wifi_ssid = promptforval("WiFi SSID")
+    if not args.wifi_password:
+        args.wifi_password = promptforval("WiFi Password")
+    print()
+    print('Using the following configuration:')
+    print('\tServer IP Address:', args.serving_host)
+    print('\tWiFi SSID:', args.wifi_ssid)
+    print('\tWiFi Password:', args.wifi_password)
+
+def stage1():
+    '''Accept the Sonoff WebSocket connection, and configure it.'''
     net_valid = False
     conn_attempt = 0
 
     if not args.no_prov:
         if not args.no_check_ip:
-            net_valid = False
             conn_attempt = 0
             # fuzzy-check if machine is connected to ITEAD AP
             while True:
                 conn_attempt += 1
-                for iface in netifaces.interfaces():
-                    # if not
-                    # netifaces.ifaddresses(iface).has_key(netifaces.AF_INET):
-                    # python2
-                    if netifaces.AF_INET not in netifaces.ifaddresses(iface):   # python3
-                        continue
-                    for afinet in netifaces.ifaddresses(iface)[netifaces.AF_INET]:
-                        # print(afinet['addr'])
-                        if afinet['addr'].startswith("10.10.7."):
-                            net_valid = True
-                            break
-                if not net_valid:
-                    try:
-                        raise Exception("IPADDR_ITEAD_NOT_ASSIGNED",
-                                        "You do not appear to be connected to the ITEAD device")
-                    except Exception as e:
-                        if(e.args[0] != "IPADDR_ITEAD_NOT_ASSIGNED"):
-                            raise
-                        if conn_attempt == 1:
-                            print(
-                                "** No ip address of the ITEAD DHCP range (10.10.7.0/24)",
-                                "is assigned to any of your interfaces,",
-                                "which means you don't appear to be connected to the IEAD WiFi network.")
-                            print(
-                                "** Please change into the ITEAD WiFi network (ITEAD-100001XXXX)")
-                            print("** This application can be kept running.")
-                        sleep(2)
-                        print(".", end="", flush=True)
-                        continue
-                else:
+                if hasfinalstageip():
+                    print("Appear to have connected to the final stage IP, "\
+                        "moving to next stage.")
+                    return
+                if hassonoffip():
                     break
+                else:
+                    if conn_attempt == 1:
+                        print("** Now connect via WiFi to your Sonoff device.")
+                        print("** Please change into the ITEAD WiFi",
+                            "network (ITEAD-100001XXXX). The default password",
+                            "is 12345678.")
+                        print("To reset the Sonoff to defaults, press",
+                            "the button for 7 seconds and the light will",
+                            "start flashing rapidly.")
+                        print("** This application should be kept running",
+                            "and will wait until connected to the Sonoff...")
+                    sleep(2)
+                    print(".", end="", flush=True)
+                    continue
 
         http = Http(timeout=2)
 
@@ -421,7 +513,7 @@ def main():
             "ssid": args.wifi_ssid,
             "password": args.wifi_password,
             "serverName": args.serving_host,
-            "port": default_port
+            "port": DEFAULT_PORT_HTTPS
         }
         print(">> HTTP POST /10.10.7.1/ap")
         print(">> %s", json.dumps(data, indent=4))
@@ -432,46 +524,35 @@ def main():
 
         print("~~ Provisioning completed")
 
+def stage2():
+    print("Starting stage2...")
+    app = make_app()
     if not args.no_check_ip:
         net_valid = False
         conn_attempt = 0
         # check if machine has <args.serving_host> being assigned on any iface
         while True:
             conn_attempt += 1
-            for iface in netifaces.interfaces():
-                # if not
-                # netifaces.ifaddresses(iface).has_key(netifaces.AF_INET): #
-                # python2
-                if netifaces.AF_INET not in netifaces.ifaddresses(iface):   # python3
-                    continue
-                for afinet in netifaces.ifaddresses(iface)[netifaces.AF_INET]:
-                    # print(afinet['addr'])
-                    if afinet['addr'] == args.serving_host:
-                        net_valid = True
-                        break
-            if not net_valid:
-                try:
-                    raise Exception(
-                        "IPADDR_SRVHOST_NOT_ASSIGNED",
-                        "The IP address of <serving_host> (%s) is not assigned to any interface" % args.serving_host)
-                except Exception as e:
-                    if(e.args[0] != "IPADDR_SRVHOST_NOT_ASSIGNED"):
-                        raise
-                    if conn_attempt == 1:
-                        print(
-                            "** The IP address of <serve_host> (%s) is not " % args.serving_host,
-                            "assigned to any interface on this machine.")
-                        print(
-                            "** Please change WiFi network to $ESSID and make ",
-                            "sure %s is being assigned to your WiFi interface.")
-                        print("** This application can be kept running.")
-                    sleep(2)
-                    print(".", end="", flush=True)
-                    continue
-            else:
+            if args.serving_host in ip4ips():
                 break
+            else:
+                if conn_attempt == 1:
+                    print(
+                        "** The IP address of <serve_host> (%s) is not " % args.serving_host,
+                        "assigned to any interface on this machine.")
+                    print(
+                        "** Please change WiFi network to %s and make sure %s is" % (
+                            args.wifi_ssid, args.serving_host),
+                        "being assigned to your WiFi interface.")
+                    print("** This application should be kept running",
+                        "and will wait until connected to the WiFi...")
+                sleep(2)
+                print(".", end="", flush=True)
+                continue
 
-    print("~~ Starting web server")
+    print("~~ Starting web server (HTTP port: %s, HTTPS port %s)" % (
+        DEFAULT_PORT_HTTP, DEFAULT_PORT_HTTPS))
+
 
     if args.legacy:
         old = make_app()
@@ -479,19 +560,69 @@ def main():
         old.listen(8081)
 
     # listening on port 8080 for serving upgrade files
-    app.listen(8080)
+    app.listen(DEFAULT_PORT_HTTP)
 
     app_ssl = tornado.httpserver.HTTPServer(app, ssl_options={
         "certfile": "ssl/server.crt",
         "keyfile": "ssl/server.key",
     })
-    # listening on port 443 to catch initial POST request to eu-disp.coolkit.cc
-    app_ssl.listen(default_port)
+    # listening on HTTPS port to catch initial POST request to eu-disp.coolkit.cc
+    app_ssl.listen(DEFAULT_PORT_HTTPS)
 
     print("~~ Waiting for device to connect")
 
+    stage3thread = threading.Thread(target=stage3, daemon=True)
+    stage3thread.start()
+
     tornado.ioloop.IOLoop.instance().start()
 
+def stage3():
+    '''This is just a thread to provide feedback to the user.'''
+    net_valid = False
+    while not net_valid:
+        print()
+        print()
+        print("*** IMPORTANT! ***")
+        print("** AFTER the first download is COMPLETE, with in a minute or so",
+            'you should connect to the new SSID "FinalStage" to finish the process.')
+        print('** ONLY disconnect when the new "FinalStage" SSID is visible',
+            'as an available WiFi network.')
+        print('This server should automatically be allocated the IP address:',
+            '192.168.4.2.')
+        print('If you have successfully connected to "FinalStage" and this is',
+            'not the IP Address you were allocated, please ensure no other',
+            'device has connected, and reboot your Sonoff.')
+
+        count = 0
+        while True:
+            count += 1
+            if count > 10:
+                break
+            if hasfinalstageip():
+                net_valid = True
+                break
+            else:
+                sleep(2)
+                print(".", end="", flush=True)
+
+    while True:
+        if not hasfinalstageip():
+            break
+        print()
+        print()
+        print()
+        print('The "FinalStage" SSID will disappear when the device has been',
+            'fully flashed and image_arduino.bin has been installed')
+        print('Once "FinalStage" has gone away, you can stop this program')
+        sleep(30)
+
+    print('No longer on "FinalStage" SSID, all done!')
+    _thread.interrupt_main()
+
+def main():
+    checkargs()
+    stage1()
+    stage2()
 
 if __name__ == '__main__':
     try:
